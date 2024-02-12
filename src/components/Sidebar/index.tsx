797421ec317f822b99e5dc26c76a6e867cde5c01
gabriel <gabrielalmeida.su@gmail.com>
2024-02-12 19:56:15 -0300

    first commit

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -252,6 +252,20 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                   </svg>
                   Consultas
                 </Link>
+                <br/>
+                <br/>
+                
+                <Link
+                  href="/sair"
+                  className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                    pathname.includes("tables") && "bg-graydark dark:bg-meta-4"
+                  }`}
+                >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
+</svg>
+                  Sair do consultorio
+                </Link>
               </li>
             </ul>
           </div>
